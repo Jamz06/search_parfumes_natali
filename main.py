96c082ec260822a499707b28da7d6e38bb46a796
Lihoded <e.vysotin@cocos.perm.ru>
2025-10-15 10:59:23 +0500

    начальный коммит, нужно тестить, короткий поиск работает хорошо

---
+++ main.py
@@ -0,0 +1,61 @@
+import time
+
+import openpyxl
+
+from duckduckgo_search import DDGS
+
+SLEEP = 1
+# С какой строки начать
+FIRST_ROW = 8
+# Колонка с текстом для поиска
+SEARCH_COLUMN = 3
+# Колонка с URL
+URL_COLUMN = 11
+# Сколько максимально нужно картинок
+MAX_RESULTS = 2
+
+def search_for_image(keyword):
+    with DDGS() as ddgs:
+        results = ddgs.images(
+            keyword,
+            max_results=MAX_RESULTS,
+            region="ru-ru"        
+        )
+        image_urls = [r['image'] for r in results]
+
+    return(image_urls)
+
+def main():
+    workbook = openpyxl.load_workbook('raw.xlsx')
+    
+    result_filename = 'results.xlsx'
+    
+    sheet = workbook.active
+    # for row in range(FIRST_ROW, sheet.max_row + 1):
+    for row in range(FIRST_ROW, 10):
+        
+        keyword = sheet.cell(row=row, column=SEARCH_COLUMN).value
+        
+        print(f'Строка {row} поиск: {keyword}')
+        try:
+            image_urls = search_for_image(keyword)
+        except Exception as err:
+            print("Произошла ошибка:")
+            print(err)
+            print("Сохраняю результат")
+            result_filename = 'results_error.xlsx'
+            break
+        
+        print(f'Найдено картинок: {image_urls}')
+        
+        # Сохранить урлы картинок
+        for url_idx in range(len(image_urls)):
+            sheet.cell(row=row, column=URL_COLUMN+url_idx).value = image_urls[url_idx]
+            
+        time.sleep(SLEEP)
+
+    workbook.save(result_filename)
+    print(f'Результат сохранен в {result_filename}')
+    
+if __name__ == '__main__':
+    main()
